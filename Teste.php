57cc070c2eb02ab0e847833b8dd2cfb4ded25f7f
rodolfoeuripedes <36815578+rodolfoeuripedes@users.noreply.github.com>
2018-03-01 11:26:24 -0300

    Teste
    Teste

---
+++ Teste.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+<title>Academia</title>
+</head>
+<frameset rows="80,*" framespacing="0" frameborder="no" border="0">
+  <frame src="menu.php" name="topFrame" scrolling="NO" noresize >
+  <frame src="principal.php" name="mainFrame">
+</frameset>
+<noframes><body>
+</body></noframes>
+</html>
